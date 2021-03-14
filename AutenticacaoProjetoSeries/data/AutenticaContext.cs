8d2afb4e4ea138946607247411820a5678c4ccb7
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-14 20:06:58 -0300

    Modificado

---
+++ AutenticacaoProjetoSeries/data/AutenticaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutenticacaoProjetoSeries.data
+{
+    public class AutenticaContext : DbContext
+    {
+        public AutenticaContext(DbContextOptions<AutenticaContext> options)
+             : base(options)
+        {
+
+        }
+    }
+}
